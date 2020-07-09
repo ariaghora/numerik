61f4d2659bd9f96c26a072721ae3f855be947bf1
Aria Ghora Prabono <hello@ghora.net>
2020-07-09 19:55:17 +0900

    Implement ReduceSum along axis.

--- numerik.pas
+++ numerik.pas
@@ -7,7 +7,7 @@ interface
 uses
   Classes, SysUtils, Math, multiarray;
 
-{ ----------- Trigonometry ----------- }
+{ ----------- Trigonometry --------------------------------------------------- }
 function _Cos(X: single; params: array of single): single;
 function _DegToRad(X: single; params: array of single): single;
 function _Sin(X: single; params: array of single): single;
@@ -15,15 +15,23 @@ function Cos(X: TMultiArray): TMultiArray; overload;
 function DegToRad(X: TMultiArray): TMultiArray; overload;
 function Sin(X: TMultiArray): TMultiArray; overload;
 
-{ ----------- Exponential ----------- }
+{ ----------- Exponential ---------------------------------------------------- }
 function _Exp(X: single; params: array of single): single;
 function Exp(X: TMultiArray): TMultiArray; overload;
 
-{ ----------- Random number generator ----------- }
+{ ----------- Random number generator ---------------------------------------- }
 function _Random(Params: array of single): single;
 function _RandG(Params: array of single): single;
+
+{ Wrapper for pascal's Random function, as a TMultiArray. }
 function Random(Shape: array of longint): TMultiArray; overload;
+
+{ Gaussian random array generator. }
 function RandG(mean, stddev: float; Shape: array of longint): TMultiArray; overload;
+
+{ Sum-reducing TMultiArray along a specified axis. This is for convenience rather
+  than performance. }
+function ReduceSum(A: TMultiArray; axis: integer): TMultiArray;
 
 implementation
 
@@ -85,6 +93,26 @@ end;
 function RandG(mean, stddev: float; Shape: array of longint): TMultiArray; overload;
 begin
   Exit(GenerateMultiArray(Shape, @_RandG, [mean, stddev]));
+end;
+
+function ReduceSum(A: TMultiArray; axis: integer): TMultiArray;
+var
+  i: longint;
+  IterIndices: array of TLongVector;
+begin
+  SetLength(IterIndices, Length(A.Indices));
+  for i := 0 to High(IterIndices) do
+    IterIndices[i] := []; // slicing by [ALL, ALL, ..., ALL]
+
+  IterIndices[axis] := [0];
+  Result := A.Slice(IterIndices);
+
+  for i := 1 to A.Shape[axis] - 1 do
+  begin
+    IterIndices[axis] := [i];
+    Result := Result + A.Slice(IterIndices);
+  end;
+  Result := Result.Contiguous;
 end;
 
 end.
