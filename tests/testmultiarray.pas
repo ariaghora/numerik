ef14f76cde8eaad5e4fedb0fdc13c62423ab7640
Aria Ghora Prabono <hello@ghora.net>
2020-07-15 02:06:06 +0900

    Use ArrayEqual for some tests

--- tests/testmultiarray.pas
+++ tests/testmultiarray.pas
@@ -36,25 +36,28 @@ implementation
 procedure TTestMultiArray.TestAssignment;
 var
   t: TDateTime;
-  A, B, C: TMultiArray;
 begin
-  M := Random([10000, 1000]);
   t := Now;
+  Write('Generating a 10000x1000 array... ');
+  M := Random([10000, 1000]);
+  WriteLn(MilliSecondsBetween(Now, t), 'ms');
+  t := Now;
+  Write('Copying array... ');
   N := M.Copy();
-  WriteLn(MilliSecondsBetween(Now, t));
+  WriteLn(MilliSecondsBetween(Now, t), 'ms');
 end;
 
 procedure TTestMultiArray.TestBroadcastMatrixScalar;
 begin
   M := TMultiArray([1, 2, 3, 4]).Reshape([2, 2]);
-  AssertTrue(VectorsEqual((M + 2).GetVirtualData, [3, 4, 5, 6]));
+  AssertTrue(VectorEqual((M + 2).GetVirtualData, [3, 4, 5, 6]));
 end;
 
 procedure TTestMultiArray.TestBroadcastReshaped;
 begin
   M := [1, 2, 3];
   M := M.Reshape([3, 1]) * M.Reshape([1, 3]);
-  AssertTrue(VectorsEqual(M.GetVirtualData, [1, 2, 3, 2, 4, 6, 3, 6, 9]));
+  AssertTrue(VectorEqual(M.GetVirtualData, [1, 2, 3, 2, 4, 6, 3, 6, 9]));
 end;
 
 procedure TTestMultiArray.TestSlicing2D;
@@ -63,14 +66,15 @@ var
 begin
   M := TMultiArray(TSingleVector(Range(1, 10))).Reshape([3, 3]);
   M := M.Slice([[0, 2], [1]]); // ==> [[2], [8]]
-  Assert(VectorsEqual(M.GetVirtualData, [2, 8]));
+  N := TMultiArray([2, 8]).Reshape([2, 1]);
+  Assert(ArrayEqual(M, N));
 end;
 
 procedure TTestMultiArray.TestSlicing2DAddScalar;
 begin
   M := TMultiArray(TSingleVector(Range(1, 10))).Reshape([3, 3]);
   M := M.Slice([[0, 2], [1]]) + 10;
-  Assert(VectorsEqual(M.GetVirtualData, [12, 18]));
+  Assert(VectorEqual(M.GetVirtualData, [12, 18]));
 end;
 
 procedure TTestMultiArray.TestSlicing2DAddTransposed;
@@ -78,28 +82,30 @@ begin
   M := TMultiArray(TSingleVector(Range(1, 10))).Reshape([3, 3]);
   M := M.Slice([[0, 2], [1]]);
   M := M.T + M;
-  AssertTrue(VectorsEqual(M.GetVirtualData, [4, 10, 10, 16]));
+  AssertTrue(VectorEqual(M.GetVirtualData, [4, 10, 10, 16]));
 end;
 
 procedure TTestMultiArray.TestTranspose;
 begin
   M := [1, 2, 3, 4, 5, 6];
   M := M.Reshape([2, 3]);
-  AssertTrue(VectorsEqual(M.T.GetVirtualData, [1, 4, 2, 5, 3, 6]));
+  N := [1, 4, 2, 5, 3, 6];
+  N := N.Reshape([3, 2]);
+  AssertTrue(ArrayEqual(M.T, N));
 end;
 
 procedure TTestMultiArray.TestReduceSimple;
 begin
   M := TMultiArray([1, 2, 3, 4, 5, 6]).Reshape([2, 3]);
   M := ReduceSum(M, 0);
-  AssertTrue(VectorsEqual(M.GetVirtualData, [5, 7, 9]));
+  AssertTrue(VectorEqual(M.GetVirtualData, [5, 7, 9]));
 end;
 
 procedure TTestMultiArray.TestReduceStacked;
 begin
   M := TMultiArray([1, 2, 3, 4, 5, 6]).Reshape([2, 3]);
   M := ReduceSum(ReduceSum(M, 0), 0);
-  AssertTrue(VectorsEqual(M.GetVirtualData, [21]));
+  AssertTrue(VectorEqual(M.GetVirtualData, [21]));
 end;
 
 procedure TTestMultiArray.TestReduceBig;
@@ -113,7 +119,7 @@ begin
   SetLength(expected, 100);
   for i := 0 to 99 do
     expected[i] := 10000;
-  AssertTrue(VectorsEqual(M.GetVirtualData, expected));
+  AssertTrue(VectorEqual(M.GetVirtualData, expected));
 end;
 
 initialization
