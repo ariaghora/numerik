c1ba4d27913d8a84b3d63f29389ec62f7c9ec8f9
Aria Ghora Prabono <hello@ghora.net>
2020-09-26 17:39:29 +0900

    Make BLAS optional

--- src/numerik.blas.pas
+++ src/numerik.blas.pas
@@ -7,20 +7,22 @@ unit numerik.blas;
 interface
 
 uses
-  multiarray;
+  dynlibs, multiarray;
 
 const
 {$IFDEF UNIX}
   {$IFDEF LINUX }
-  LIB_NAME = 'libopenblas.so';
+  BLAS_LIB_NAME = 'libopenblas.so';
+  LAPACKE_LIB_NAME = 'liblapacke.so';
   {$ENDIF}
   {$IFDEF DARWIN}
-  LIB_NAME = 'libopenblas.dylib';
+  BLASLIB_NAME = 'libopenblas.dylib';
   {$ENDIF}
 {$ENDIF}
 
 {$IFDEF WINDOWS}
-  LIB_NAME = 'libopenblas.dll';
+  BLAS_LIB_NAME = 'libopenblas.dll';
+  LAPACKE_LIB_NAME = BLAS_LIB_NAME;
 {$ENDIF}
 
   LAPACK_ROW_MAJOR = 101;
@@ -31,25 +33,40 @@ const
   CBLAS_TRANS      = 112;
   CBLAS_CONJ_TRANS = 113;
 
+type
+  TCblasSgemm = procedure(Order: longint; TransA: longint; TransB: longint;
+    M: longint; N: longint; K: longint; alpha: single; A: TSingleVector;
+    lda: longint; B: TSingleVector; ldb: longint; beta: single;
+    C: TSingleVector; ldc: longint); cdecl;
+  TCblasSaxpy = procedure(N: longint; A: single; X: TSingleVector; INCX: longint;
+    Y: TSingleVector; INCY: longint); cdecl;
+  TLAPACKESgeSVD = function(MatrixLayout: longint; JOBU, JOBVT: char;
+    M, N: longint; A: TSingleVector; LDA: longint; S, U: TSingleVector;
+    LDU: longint; VT: TSingleVector; LDVT: longint; Superb: TSingleVector): longint; cdecl;
+
+var
+  cblas_saxpy: TCblasSaxpy;
+  cblas_sgemm: TCblassgemm;
+  LAPACKE_sgesvd: TLAPACKESgeSVD;
+  BLASlibHandle: THandle = NilHandle;
+  LAPACKElibHandle: THandle = NilHandle;
+
 function Add_BLAS(A, B: TMultiArray): TMultiArray;
+//function Add_Native(A, B: TMultiArray): TMultiArray;
+function Sub_BLAS(A, B: TMultiArray): TMultiArray;
+//function Sub_Native(A, B: TMultiArray): TMultiArray;
 function MatMul_BLAS(A, B: TMultiArray): TMultiArray;
+function MatMul_Native(A, B: TMultiArray): TMultiArray;
 
-procedure cblas_saxpy(N: longint; A: single; X: TSingleVector; INCX: longint;
-  Y: TSingleVector; INCY: longint);
-  external LIB_NAME;
+function MatMul_BACKEND(A, B: TMultiArray): TMultiArray;
 
-procedure cblas_sgemm(Order: longint; TransA: longint; TransB: longint;
-  M: longint; N: longint; K: longint; alpha: single; A: TSingleVector;
-  lda: longint; B: TSingleVector; ldb: longint; beta: single;
-  C: TSingleVector; ldc: longint);
-  external LIB_NAME;
-
-function LAPACKE_sgesvd(MatrixLayout: longint; JOBU, JOBVT: char;
-  M, N: longint; A: TSingleVector; LDA: longint; S, U: TSingleVector;
-  LDU: longint; VT: TSingleVector; LDVT: longint; Superb: TSingleVector): longint;
-  external {$IFDEF WINDOWS}LIB_NAME{$ENDIF}{$IFDEF UNIX}'liblapacke.so'{$ENDIF};
+function IsBLASAvailable: boolean;
+function IsLAPACKEAvailable: boolean;
 
 implementation
+
+uses
+  numerik;
 
 function Add_BLAS(A, B: TMultiArray): TMultiArray;
 begin
@@ -60,6 +77,18 @@ begin
   Result.Data := CopyVector(B.GetVirtualData);
   cblas_saxpy(A.Size, 1, A.GetVirtualData, 1, Result.Data, 1);
 end;
+
+
+function Sub_BLAS(A, B: TMultiArray): TMultiArray;
+begin
+  Result := AllocateMultiArray(A.Size,
+                               False    // do not allocate the memory yet
+                               ).Reshape(A.Shape);
+  Result.Data := nil;
+  Result.Data := CopyVector(A.GetVirtualData);
+  cblas_saxpy(A.Size, -1, B.GetVirtualData, 1, Result.Data, 1);
+end;
+
 
 function MatMul_BLAS(A, B: TMultiArray): TMultiArray;
 var
@@ -77,6 +106,51 @@ begin
     Result.Data, B.Shape[1]             // C, B.shape[1]
     );
 end;
+
+function MatMul_Native(A, B: TMultiArray): TMultiArray;
+var
+  i, j, k: longint;
+  sum: double;
+begin
+  Result := AllocateMultiArray(A.Shape[0] * B.Shape[1]).Reshape([A.Shape[0], B.Shape[1]]);
+  //SetLength(Result.Val, A.Shape[0] * B.Shape[1]);
+  for i := 0 to A.shape[0] - 1 do
+    for j := 0 to B.Shape[1] - 1 do
+    begin
+      sum := 0;
+      for k := 0 to A.Shape[1] - 1 do
+        sum := sum + A.Get(i * A.Shape[1] + k) * B.Get(k * B.Shape[1] + j);
+      Result.Data[i * B.Shape[1] + j] := sum;
+    end;
+  //Result.Reshape([A.Shape[0], B.Shape[1]]);
+end;
+
+function MatMul_BACKEND(A, B: TMultiArray): TMultiArray;
+begin
+  if cblas_sgemm <> nil then
+    Exit(MatMul_BLAS(A, B))
+  else
+    Exit(MatMul_Native(A, B));
+end;
+
+function IsBLASAvailable: boolean;
+begin
+  Exit(BLASlibHandle <> NilHandle);
+end;
+
+function IsLAPACKEAvailable: boolean;
+begin
+  Exit(LAPACKElibHandle <> NilHandle);
+end;
+
+initialization
+
+BLASlibHandle := LoadLibrary(BLAS_LIB_NAME);
+LAPACKElibHandle := LoadLibrary(LAPACKE_LIB_NAME);
+
+cblas_sgemm := TCblassgemm(GetProcedureAddress(BLASlibHandle, 'cblas_sgemm'));
+cblas_saxpy := TCblasSaxpy(GetProcedureAddress(BLASlibHandle, 'cblas_saxpy'));
+LAPACKE_sgesvd := TLAPACKESgeSVD(GetProcedureAddress(LAPACKElibHandle, 'LAPACKE_sgesvd'));
 
 end.
 
